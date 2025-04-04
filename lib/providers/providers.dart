eeb22e732b16f3e8a19fc47e53a657fe431129ef
Aristia <aristiagordillo@gmail.com>
2025-04-04 19:29:14 +0000

    Login with Bloc & Freezed

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:qribar_cocina/data/datasources/remote_data_source/listeners_data_source.dart';
-import 'package:qribar_cocina/presentation/login/provider/login_form_provider.dart';
+import 'package:qribar_cocina/presentation/login/bloc/login_form_bloc.dart';
 import 'package:qribar_cocina/providers/navegacion_provider.dart';
 import 'package:qribar_cocina/providers/products_provider.dart';
 
-/// A [StatelessWidget] that wraps the app with the necessary providers.
 class AppProviders extends StatelessWidget {
   final Widget child;
 
@@ -17,10 +17,18 @@ class AppProviders extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (_) => ProductsService()),
         ChangeNotifierProvider(create: (_) => NavegacionProvider()),
-        ChangeNotifierProvider(create: (_) => LoginFormProvider()),
         ChangeNotifierProvider(create: (_) => ListenersDataSource()),
       ],
-      child: child,
+      child: Builder(
+        builder: (context) {
+          final productsService = context.read<ProductsService>();
+
+          return BlocProvider(
+            create: (_) => LoginFormBloc(productsService: productsService),
+            child: child,
+          );
+        },
+      ),
     );
   }
 }
