c301f9a5652bff312aa28dc3fc0294f9451de5c3
Aristia <aristiagordillo@gmail.com>
2025-04-20 18:26:50 +0000

    Separate models

--- lib/data/models/product.dart
+++ lib/data/models/product.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:qribar_cocina/data/models/complementos/complementos.dart';
 import 'package:qribar_cocina/data/models/modifier/modifier.dart';
 
 class Product {
@@ -158,32 +159,4 @@ class Product {
       complementos: this.complementos,
       modifiers: this.modifiers,
       id: this.id);
-}
-
-class Complemento {
-  Complemento({
-    this.activo,
-    this.incremento,
-    this.nombre,
-    this.id,
-  });
-
-  bool? activo;
-  bool? incremento;
-  String? id;
-  String? nombre;
-
-  factory Complemento.fromJson(String str) => Complemento.fromMap(json.decode(str));
-
-  String toJson() => json.encode(toMap());
-
-  factory Complemento.fromMap(Map<String, dynamic> json) => Complemento(
-        activo: json["activo"],
-        incremento: json["incremento"],
-      );
-
-  Map<String, dynamic> toMap() => {
-        "activo": activo,
-        "incremento": incremento,
-      };
 }
