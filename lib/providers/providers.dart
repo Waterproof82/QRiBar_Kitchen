96b5443b042868b3468658341c679e650155caaf
Aristia <aristiagordillo@gmail.com>
2025-04-20 18:26:23 +0000

    Delete use of productsService

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -7,7 +7,6 @@ import 'package:qribar_cocina/data/repositories/data_sources/remote/listener_rep
 import 'package:qribar_cocina/presentation/login/bloc/login_form_bloc.dart';
 import 'package:qribar_cocina/providers/bloc/listener_bloc.dart';
 import 'package:qribar_cocina/providers/navegacion_provider.dart';
-import 'package:qribar_cocina/providers/products_provider.dart';
 
 class AppProviders extends StatelessWidget {
   final Widget child;
@@ -18,29 +17,32 @@ class AppProviders extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => ProductsService()),
         ChangeNotifierProvider(create: (_) => NavegacionProvider()),
       ],
       child: Builder(
         builder: (context) {
-          final productsService = context.read<ProductsService>();
           final database = FirebaseDatabase.instance;
+          final navProvider = context.read<NavegacionProvider>();
 
           final listenerDataSource = ListenersDataSourceImpl(
             database: database,
-            productService: productsService,
+            navProvider: navProvider,
           );
-          final listenerRepo = ListenerRepositoryImpl(database: database, dataSource: listenerDataSource);
+          final listenerRepo = ListenerRepositoryImpl(
+            database: database,
+            dataSource: listenerDataSource,
+          );
 
           final listenerBloc = ListenerBloc(repository: listenerRepo);
 
           return MultiBlocProvider(
             providers: [
+              BlocProvider(create: (_) => LoginFormBloc()),
               BlocProvider(
-                create: (_) => LoginFormBloc(productsService: productsService),
-              ),
-              BlocProvider(
-                create: (_) => listenerBloc..add(const ListenerEvent.startListening()),
+                create: (_) => listenerBloc
+                  ..add(
+                    const ListenerEvent.startListening(),
+                  ),
               ),
             ],
             child: child,
