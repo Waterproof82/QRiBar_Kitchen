f3e13910eb6064b6f97411c809dedd904358a296
Aristia <aristiagordillo@gmail.com>
2025-05-08 09:28:28 +0000

    Improve bootstrap loading & Add Dynatrace

--- lib/core/bootstrap.dart
+++ lib/core/bootstrap.dart
@@ -2,15 +2,11 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-import 'package:intl/date_symbol_data_local.dart';
-import 'package:qribar_cocina/data/config/svg/precache.dart';
 import 'package:qribar_cocina/data/types/errors/network_error.dart';
 import 'package:qribar_cocina/data/types/repository_error.dart';
 import 'package:qribar_cocina/data/types/result.dart';
-import 'package:wakelock_plus/wakelock_plus.dart';
+import 'package:qribar_cocina/di/di.dart';
 
 /// A [BlocObserver] which observes all [Bloc] state changes.
 class AppBlocObserver extends BlocObserver {
@@ -37,23 +33,13 @@ Future<Result<void>> bootstrap(FutureOr<Widget> Function() builder) async {
   };
   WidgetsFlutterBinding.ensureInitialized();
 
+  Bloc.observer = const AppBlocObserver();
+
   try {
-    await Future.wait([
-      Firebase.initializeApp(),
-      initializeDateFormatting('es_ES'),
-      WakelockPlus.enable(),
-      SystemChrome.setPreferredOrientations([
-        DeviceOrientation.portraitUp,
-        DeviceOrientation.portraitDown,
-      ]),
-      precacheSVGs(),
-    ]);
-
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [SystemUiOverlay.bottom]);
-
-    Bloc.observer = const AppBlocObserver();
+    await initDi();
 
     runApp(await builder());
+
     return const Result.success(null);
   } catch (e, stackTrace) {
     log('Error during app bootstrap: $e', stackTrace: stackTrace);
