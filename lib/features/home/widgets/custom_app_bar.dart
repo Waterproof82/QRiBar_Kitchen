6168a7c2c2ce11e1403a9cb1500e5f2ad45e2268
Aristia <aristiagordillo@gmail.com>
2025-07-08 12:04:17 +0000

    Remove ValueNotifier & Improve with Selector & Refactor

--- lib/features/home/widgets/custom_app_bar.dart
+++ lib/features/home/widgets/custom_app_bar.dart
@@ -14,15 +14,10 @@ import 'package:qribar_cocina/shared/utils/language_dropdown.dart';
 /// adapting its title size based on screen width.
 final class CustomAppBar extends StatelessWidget
     implements PreferredSizeWidget {
-  /// Defines the preferred height of the AppBar.
+  const CustomAppBar({super.key});
+
   @override
   Size get preferredSize => const Size.fromHeight(60);
-
-  /// A [ValueNotifier] to control the overall visibility of the navigation rail.
-  final ValueNotifier<bool> isRailVisible;
-
-  /// Creates a constant instance of [CustomAppBar].
-  const CustomAppBar({super.key, required this.isRailVisible});
 
   @override
   Widget build(BuildContext context) {
@@ -32,43 +27,38 @@ final class CustomAppBar extends StatelessWidget
       toolbarHeight: 60,
       backgroundColor: AppColors.black,
       iconTheme: const IconThemeData(color: AppColors.onPrimary),
-      leading: IconButton(
-        icon: ValueListenableBuilder<bool>(
-          valueListenable: isRailVisible,
-          builder: (context, isVisible, child) {
-            return AnimatedRotation(
-              turns: isVisible ? 0.25 : 0.0, // Rotate 90 degrees when hidden
-              duration: const Duration(milliseconds: 300),
-              child: Icon(
-                isVisible ? Icons.menu_outlined : Icons.menu,
-                color: AppColors.onPrimary,
-              ),
-            );
-          },
-        ),
-        onPressed: () {
-          isRailVisible.value = !isRailVisible.value;
+      leading: Selector<NavigationProvider, bool>(
+        selector: (_, provider) => provider.isRailVisible,
+        builder: (context, isRailVisible, _) {
+          return IconButton(
+            icon: Icon(
+              isRailVisible ? Icons.menu_outlined : Icons.menu,
+              color: AppColors.onPrimary,
+            ),
+            onPressed: () {
+              context.read<NavigationProvider>().isRailVisible = !isRailVisible;
+            },
+          );
         },
       ),
       title: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Flexible(
-            child: Consumer<NavigationProvider>(
-              builder: (context, nav, child) {
-                String displayedLabel =
-                    context.l10n.generalView; // Default label
+            child: Selector<NavigationProvider, String>(
+              selector: (_, provider) => provider.categoriaSelected,
+              builder: (context, categoriaSelected, _) {
+                String displayedLabel = context.l10n.generalView;
 
                 try {
-                  final SelectionTypeEnum selectionType = SelectionTypeEnum
-                      .values
-                      .firstWhere((e) => e.name == nav.categoriaSelected);
-                  displayedLabel = selectionType.localizedLabel(context);
-                } catch (e) {
-                  debugPrint(
-                    'Warning: nav.categoriaSelected "${nav.categoriaSelected}" not found in SelectionTypeEnum. Falling back to default label.',
+                  final selectionType = SelectionTypeEnum.values.firstWhere(
+                    (e) => e.name == categoriaSelected,
                   );
-                  displayedLabel = context.l10n.generalView;
+                  displayedLabel = selectionType.localizedLabel(context);
+                } catch (_) {
+                  debugPrint(
+                    'Warning: nav.categoriaSelected "$categoriaSelected" not found in SelectionTypeEnum. Using default.',
+                  );
                 }
 
                 return FittedBox(
@@ -77,7 +67,7 @@ final class CustomAppBar extends StatelessWidget
                     displayedLabel,
                     style: GoogleFonts.poiretOne(
                       color: AppColors.onPrimary,
-                      fontSize: (screenWidthSize > 450) ? 35 : 28,
+                      fontSize: screenWidthSize > 450 ? 35 : 28,
                       fontWeight: FontWeight.w600,
                     ),
                     overflow: TextOverflow.ellipsis,
